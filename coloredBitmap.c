c2fe00a9efeeaf70c60b6aa640536c1208b5df05
ferieltalidj@gmail.com <ferieltalidj@gmail.com>
2024-11-22 17:22:09 +0100

    code-added

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,23 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+float sizeinbits = (float)(w*h*24);
+   if (strcmp(unit, "bt") == 0)
+   {
+      return sizeinbits;
+   }
+   else if (strcmp(unit, "ko") == 0) 
+   {
+      return sizeInBits / 1024;     
+   }
+   else if (strcmp(unit, "mo") == 0) 
+   {
+      return sizeInBits / (1024 * 1024); 
+   } 
+   else if (strcmp(unit, "go") == 0) 
+   {
+      return sizeInBits / (1024 * 1024 * 1024);     
+   }
    // YOUR CODE HERE - END
    return 0;
 }
--- video.c
+++ video.c
@@ -10,6 +10,30 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+float coloredFrameSizeBits = 24 * w * h; 
+    float bwFrameSizeBits = 8 * w * h;   
+
+        float coloredSectionBits = fps * durationMovie * coloredFrameSizeBits;
+    float bwSectionBits = fps * durationCredits * bwFrameSizeBits;
+
+        float totalSizeBits = coloredSectionBits + bwSectionBits;
+
+        if (strcmp(unit, "bt") == 0) 
+  {
+        return totalSizeBits;   
+  } 
+else if (strcmp(unit, "ko") == 0) 
+  {
+        return totalSizeBits / 1024;    
+  } 
+else if (strcmp(unit, "mo") == 0) 
+  {
+        return totalSizeBits / (1024 * 1024);     
+  } 
+else if (strcmp(unit, "go") == 0) 
+  {
+        return totalSizeBits / (1024 * 1024 * 1024);
+  }
 
    // YOUR CODE HERE - END
    return 0;
